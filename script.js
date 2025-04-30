c530cb195fec9e5f7e342ed7825f22c3bc1d4783
Jade Clitar <clitarjade@gmail.com>
2025-05-01 01:10:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,79 @@
-const inputBox = document.querySelector('#input-box');
-const listItems = document.querySelector('.list-items');
-const addButton = document.querySelector('.add-btn');
-const listButton = document.querySelector('.list-item-btn');
-const list = document.querySelector('span');
+const inputBox = document.querySelector("#input-box");
+const listItems = document.querySelector(".list-items");
+const addButton = document.querySelector(".add-btn");
+const listButton = document.querySelector(".list-item-btn");
+const list = document.querySelector("span");
 
+addButton.addEventListener("click", () => {
+	const inputValue = inputBox.value;
+	listItems.insertAdjacentHTML(
+		"beforeend",
+		`
+		 <li class="todo-list">
+			<span class="input-value">${inputValue}</span>
+			<div class="todo-btns">
+				<button class="list-item-btn"><img src="images/edit.svg" class="btn-icon edit-btn" alt="edit-icon"></button>
+				<button class="list-item-btn"><img src="images/delete.svg" class="btn-icon delete-btn" alt="delete-icon"></button>
+			</div>
+		</li>
+		`
+	);
 
-addButton.addEventListener('click', () => {
-    const inputValue = inputBox.value;
-    listItems.insertAdjacentHTML('beforeend', 
-    `
-    <li class="todo-list">
-        <span>${inputValue}</span>
-        <button class="list-item-btn" onclick="this.parentNode.remove();">x</button>
-    </li>
-    `);
-
-    inputBox.value = '';
-    inputBox.blur();
-    saveData();
+	inputBox.value = "";
+	inputBox.blur();
+	saveData();
 });
 
 inputBox.addEventListener("keydown", (event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        addButton.click();
-    }
+	if (event.key === "Enter") {
+		event.preventDefault();
+		addButton.click();
+	}
 });
 
+listItems.addEventListener("click", (e) => {
+	if (e.target.classList.contains("edit-btn")) {
+		const spanTarget = e.target.parentElement.parentElement;
+		const spanValue = spanTarget.parentElement.querySelector(".input-value");
+		const imgTarget = e.target.parentElement;
+		const imgBtn = imgTarget.querySelector(".edit-btn");
+		let imgSrc = imgTarget.querySelector(".edit-btn");
+		let altName = imgTarget.querySelector(".edit-btn");
+		console.log(imgSrc.src);
+		if (altName.alt === "edit-icon") {
+			spanValue.setAttribute("contenteditable", "true");
+			spanValue.focus();
+			imgSrc.src = "./images/save.svg";
+			altName.alt = "save-icon";
+			console.log(imgSrc);
+		} else if (altName.alt === "save-icon") {
+			spanValue.setAttribute("contenteditable", "false");
+			imgSrc.src = "./images/edit.svg";
+			altName.alt = "edit-icon";
+		}
 
-listItems.addEventListener('click', (event) => {
-    if(event.target.tagName === "LI") {
-        event.target.classList.toggle('checked');
-        saveData();
-    } else if(event.target.className === 'list-item-btn') {
-        event.target.parentElement.remove();
-        saveData();
-    }
-}, false);
+		spanValue.addEventListener("keydown", (e) => {
+			if (e.key === "Enter") {
+				e.preventDefault();
+				imgBtn.click();
+			}
+		});
+
+		saveData();
+	} else if (e.target.classList.contains("delete-btn")) {
+		const todoListTarget = e.target.parentElement.parentElement;
+		todoListTarget.parentElement.remove();
+
+		saveData();
+	}
+});
 
 function saveData() {
-    localStorage.setItem('data', listItems.innerHTML);
+	localStorage.setItem("data", listItems.innerHTML);
 }
 
 function showTask() {
-    listItems.innerHTML = localStorage.getItem('data');
+	listItems.innerHTML = localStorage.getItem("data");
 }
 
 showTask();
-
-
-
-
-
-
